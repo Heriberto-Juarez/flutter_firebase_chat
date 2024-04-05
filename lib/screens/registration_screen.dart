729cadc805884d4e5d442aa01ff8e5f05fa4a0a2
Heriberto-Juarez <heribertojuarezj@gmail.com>
2024-04-04 21:58:45 -0600

    Add loader to register screen too

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = "registration_screen";
@@ -16,66 +17,70 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   String email = "";
   String password = "";
+  bool _saving = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: "flash_logo",
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: "flash_logo",
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kRoundedInputDecoration.copyWith(hintText: 'Enter your email'),
-              style: kInputStyle,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kRoundedInputDecoration.copyWith(hintText: 'Enter your password'),
-              style: kInputStyle,
-              textAlign: TextAlign.center,
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            PaddingButton(buttonText: 'Register', color: Colors.blueAccent, onPressed: () async {
-              print(email);
-              print(password);
-              try {
-                final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                print(newUser);
-                if (newUser != null){
-                  Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kRoundedInputDecoration.copyWith(hintText: 'Enter your email'),
+                style: kInputStyle,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kRoundedInputDecoration.copyWith(hintText: 'Enter your password'),
+                style: kInputStyle,
+                textAlign: TextAlign.center,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              PaddingButton(buttonText: 'Register', color: Colors.blueAccent, onPressed: () async {
+                print(email);
+                print(password);
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                  print(newUser);
+                  if (newUser != null){
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                }catch(e){
+                  print("==== Error ====");
+                  print(e);
                 }
-              }catch(e){
-                print("==== Error ====");
-                print(e);
-              }
-            }),
-          ],
+              }),
+            ],
+          ),
         ),
+        inAsyncCall: _saving,
       ),
     );
   }
